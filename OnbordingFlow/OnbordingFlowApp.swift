f41c6aa2ace9ffc4387ec42929352ffadfeecfab
Stas <ragioniere1980@gmail.com>
2025-06-21 00:14:35 +0300

    first

---
+++ OnbordingFlow/OnbordingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnbordingFlowApp.swift
+//  OnbordingFlow
+//
+//  Created by Станислав Леонов on 21.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct OnbordingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
